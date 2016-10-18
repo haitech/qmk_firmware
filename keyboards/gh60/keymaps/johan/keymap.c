8f7805a64275e22b8706bdddbd829a5f112a410c
johsoren <johan.sorensen@finn.no>
2016-10-18 08:23:18 +0200

    Duplicate mouse keys to arrow layer

--- keyboards/gh60/keymaps/johan/keymap.c
+++ keyboards/gh60/keymaps/johan/keymap.c
@@ -8,6 +8,7 @@ enum keyboard_layers_macros {
   // Macros
   MACRO_N
 };
+
 #define SPC_FN LT(LAYER_ARROW, KC_SPC)
 
 // Fillers to make layering more clear
@@ -26,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 1: fn0 */
 	[LAYER_ARROW] = {
     { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL   },
-    { _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, },
-    { _______, _______, _______, _______, _______, _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, XXXXXXX, _______, },
-    { _______, XXXXXXX, _______, _______, _______, _______, _______, M(MACRO_N)   , _______, _______, _______, _______, XXXXXXX, KC_RSFT, },
+    { _______, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_D, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, },
+    { _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, _______, KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, XXXXXXX, _______, },
+    { KC_LSFT, XXXXXXX, _______, _______, _______, _______, _______, M(MACRO_N)   , _______, _______, _______, _______, XXXXXXX, KC_RSFT, },
     { _______, _______, _______, XXXXXXX, XXXXXXX, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______, _______  }
   },
     /* 2: fn1 */
